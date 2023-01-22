1bb1b0c026ae22733deb3777b8483f3ce547ec8e
ISnortPennies <ISnortPennies@protonmail.com>
2023-01-22 14:07:17 -0500

    [DWM] added focus-on-click

--- config.def.h
+++ config.def.h
@@ -7,6 +7,7 @@ static const unsigned int gappx     = 10;       /* default gap between windows i
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "monospace:size=10" };
