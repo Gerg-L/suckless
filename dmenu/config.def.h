a31720e840a786f7b4e53c15df2ef2863300d5a7
Gerg-L <GregLeyda@proton.me>
2023-03-22 00:06:19 -0400

    [DMENU] theming

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -2,21 +2,21 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 750;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#a1aab8", "#000000" },
+	[SchemeSel] = { "#000000", "#80a0ff" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 15;
 
 /*
  * Characters not considered part of a word while deleting words
