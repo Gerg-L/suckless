47d879d0e5146359f6e770b812646475603417fe
Gerg-L <GregLeyda@proton.me>
2024-08-04 21:41:05 -0400

    update and minimize

--- flake.nix
+++ flake.nix
@@ -5,34 +5,17 @@
     repo = "nixpkgs";
     ref = "nixos-unstable";
   };
-
   outputs =
-    { nixpkgs, self }:
-    let
-      inherit (nixpkgs) lib;
-      withSystem =
-        f:
-        lib.fold lib.recursiveUpdate { } (
-          map f [
-            "x86_64-linux"
-            "x86_64-darwin"
-            "aarch64-linux"
-            "aarch64-darwin"
-          ]
-        );
-      mkPackages = pkgs: {
-        dwm = pkgs.callPackage ./dwm.nix { };
-        st = pkgs.callPackage ./st.nix { };
-        dmenu = pkgs.callPackage ./dmenu.nix { };
-      };
-    in
-    withSystem (system: {
-      overlays = {
-        default = self.overlays.override;
-        override = final: _: mkPackages final;
-        insert = final: _: self.packages.${final.stdenv.hostPlatform.system};
-      };
-
-      packages.${system} = mkPackages nixpkgs.legacyPackages.${system};
-    });
+    { nixpkgs, ... }:
+    {
+      packages.x86_64-linux =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        {
+          dwm = pkgs.callPackage ./dwm.nix { };
+          st = pkgs.callPackage ./st.nix { };
+          dmenu = pkgs.callPackage ./dmenu.nix { };
+        };
+    };
 }
