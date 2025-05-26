ebedaf1b454ef1da343a96f63dc95f1cd7aa57f3
Gerg-L <GregLeyda@proton.me>
2025-05-26 14:43:05 -0400

    [DWM] use ghostty

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "ghostty", NULL };
 static const char *quitcmd[]  = { "pkill", "dwm", NULL };
 
 #include "movestack.c"
